022cd0ba308fe2d2568477be60f2b562ce9b78ed
Langoni-ops <63052568+Langoni-ops@users.noreply.github.com>
2020-11-07 20:46:38 -0300

    Create InsertConversation.php

---
+++ InsertConversation.php
@@ -0,0 +1,13 @@
+INSERT INTO `chatbot_hints` (`id`, `question`, `reply`) VALUES
+(11,'Bom dia', 'Bom dia, tudo bem? Em que posso ajudar?');
+(12,'Bom tarde', 'Bom tarde, tudo bem? Em que posso ajudar?');
+(13,'Bom noite', 'Bom noite, tudo bem? Em que posso ajudar?');
+(14,'Não estou bem','Fico chateado em saber, gostaria de conversar?');
+(15,'Gostaria de conversar', 'Como você está se sentindo ?');
+(16,'Triste', "O que aconteceu?',);
+(17,'Briguei com minha familia', 'Quer desabafar');
+(18,'Sim', 'Certo, estou aqui para te ouvir');
+(19,'Não, 'Ok, Quando quiser estarei aqui para ter ouvir');
+(20,'Bravo', 'O que aconteceu?' );
+(21,'Bati meu carro', 'Quer conversar ?');
+(22,'Pensativo', 'No que esta pensando ?');
